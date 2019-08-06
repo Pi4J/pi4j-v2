70e02b086363783e72d6f275d2fcf22bb676924d
Robert Savage <robert@shadeblue.com>
2019-08-06 17:19:33 -0400

    consolidated 'platforms' and 'providers' into a singular 'plugins' directory and Maven project structure

---
+++ plugins/pi4j-plugin-raspberrypi/src/main/java/com/pi4j/raspberrypi/platform/RaspberryPiPlatform.java
@@ -0,0 +1,78 @@
+package com.pi4j.raspberrypi.platform;
+
+/*-
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: PLUGIN   :: Raspberry Pi Plugin
+ * FILENAME      :  RaspberryPiPlatform.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  https://pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2019 Pi4J
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import com.pi4j.context.Context;
+import com.pi4j.platform.Platform;
+import com.pi4j.platform.PlatformBase;
+import com.pi4j.raspberrypi.RaspberryPi;
+import com.pi4j.raspberrypi.provider.gpio.digital.RpiDigitalInputProvider;
+import com.pi4j.raspberrypi.provider.gpio.digital.RpiDigitalOutputProvider;
+import com.pi4j.raspberrypi.provider.i2c.RpiI2CProvider;
+import com.pi4j.raspberrypi.provider.pwm.RpiPwmProvider;
+import com.pi4j.raspberrypi.provider.serial.RpiSerialProvider;
+import com.pi4j.raspberrypi.provider.spi.RpiSpiProvider;
+
+public class RaspberryPiPlatform extends PlatformBase<RaspberryPiPlatform> implements Platform {
+
+
+    public RaspberryPiPlatform(){
+        super(RaspberryPi.PLATFORM_ID,
+                RaspberryPi.PLATFORM_NAME,
+                RaspberryPi.PLATFORM_DESCRIPTION);
+    }
+
+    @Override
+    public int weight() {
+        // the MOCK platform is weighted at zero to indicate that it has a very
+        // low priority and should only be used in the case where other platforms
+        // are not found in the classpath
+        return 5;
+    }
+
+    @Override
+    public boolean enabled(Context context) {
+        // the Mock Platform is always available when detected
+        // there are no logic checked required to determine when
+        // and if the mock platforms should be enabled
+        return true;
+    }
+
+    @Override
+    protected String[] getProviders() {
+        return new String[] {
+                RpiDigitalInputProvider.ID,
+                RpiDigitalOutputProvider.ID,
+                RpiPwmProvider.ID,
+                RpiI2CProvider.ID,
+                RpiSpiProvider.ID,
+                RpiSerialProvider.ID};
+    }
+}
