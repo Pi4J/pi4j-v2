d12d6bdf3825c9e1f41274290bd9357984cb23e6
Robert Savage <robert@shadeblue.com>
2019-09-06 14:59:21 -0400

    added convenience 'create()' methods for config builder instances in main context

--- pi4j-api/src/main/java/com/pi4j/context/Context.java
+++ pi4j-api/src/main/java/com/pi4j/context/Context.java
@@ -36,15 +36,19 @@ import com.pi4j.io.gpio.analog.*;
 import com.pi4j.io.gpio.digital.*;
 import com.pi4j.io.i2c.I2C;
 import com.pi4j.io.i2c.I2CConfig;
+import com.pi4j.io.i2c.I2CConfigBuilder;
 import com.pi4j.io.i2c.I2CProvider;
 import com.pi4j.io.pwm.Pwm;
 import com.pi4j.io.pwm.PwmConfig;
+import com.pi4j.io.pwm.PwmConfigBuilder;
 import com.pi4j.io.pwm.PwmProvider;
 import com.pi4j.io.serial.Serial;
 import com.pi4j.io.serial.SerialConfig;
+import com.pi4j.io.serial.SerialConfigBuilder;
 import com.pi4j.io.serial.SerialProvider;
 import com.pi4j.io.spi.Spi;
 import com.pi4j.io.spi.SpiConfig;
+import com.pi4j.io.spi.SpiConfigBuilder;
 import com.pi4j.io.spi.SpiProvider;
 import com.pi4j.platform.Platform;
 import com.pi4j.platform.Platforms;
@@ -532,6 +536,94 @@ public interface Context extends Describable {
                 throw new ProviderNotFoundException(providerId);
             }
         }
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.gpio.analog.AnalogOutputConfigBuilder} object.
+     * @return a {@link com.pi4j.io.gpio.analog.AnalogOutput} object.
+     * @throws java.lang.Exception if any.
+     */
+    default AnalogOutput create(AnalogOutputConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.gpio.analog.AnalogInputConfigBuilder} object.
+     * @return a {@link com.pi4j.io.gpio.analog.AnalogInput} object.
+     * @throws java.lang.Exception if any.
+     */
+    default AnalogInput create(AnalogInputConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder} object.
+     * @return a {@link com.pi4j.io.gpio.digital.DigitalOutput} object.
+     * @throws java.lang.Exception if any.
+     */
+    default DigitalOutput create(DigitalOutputConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.gpio.digital.DigitalInputConfigBuilder} object.
+     * @return a {@link com.pi4j.io.gpio.digital.DigitalInput} object.
+     * @throws java.lang.Exception if any.
+     */
+    default DigitalInput create(DigitalInputConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.pwm.PwmConfigBuilder} object.
+     * @return a {@link com.pi4j.io.pwm.Pwm} object.
+     * @throws java.lang.Exception if any.
+     */
+    default Pwm create(PwmConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.i2c.I2CConfigBuilder} object.
+     * @return a {@link com.pi4j.io.i2c.I2C} object.
+     * @throws java.lang.Exception if any.
+     */
+    default I2C create(I2CConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.spi.SpiConfigBuilder} object.
+     * @return a {@link com.pi4j.io.spi.Spi} object.
+     * @throws java.lang.Exception if any.
+     */
+    default Spi create(SpiConfigBuilder config) throws Exception{
+        return create(config.build());
+    }
+
+    /**
+     * <p>create.</p>
+     *
+     * @param config a {@link com.pi4j.io.serial.SerialConfigBuilder} object.
+     * @return a {@link com.pi4j.io.serial.Serial} object.
+     * @throws java.lang.Exception if any.
+     */
+    default Serial create(SerialConfigBuilder config) throws Exception{
+        return create(config.build());
     }
 
     /**
