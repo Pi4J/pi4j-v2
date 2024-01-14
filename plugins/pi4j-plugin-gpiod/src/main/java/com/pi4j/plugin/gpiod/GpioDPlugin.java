a552d7a7eb7629fdeabb2d6c67684f8a8b791d7b
alex9849 <alexander@liggesmeyer.net>
2024-01-14 20:33:17 +0100

    Added some base classes

---
+++ plugins/pi4j-plugin-gpiod/src/main/java/com/pi4j/plugin/gpiod/GpioDPlugin.java
@@ -0,0 +1,45 @@
+package com.pi4j.plugin.gpiod;
+
+import com.pi4j.extension.Plugin;
+import com.pi4j.extension.PluginService;
+import com.pi4j.plugin.gpiod.provider.gpio.digital.GpioDDigitalOutputProvider;
+import com.pi4j.provider.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GpioDPlugin implements Plugin {
+
+    /**
+     * Constant <code>NAME="GpioD"</code>
+     */
+    public static final String NAME = "GpioD";
+    /**
+     * Constant <code>ID="gpiod"</code>
+     */
+    public static final String ID = "gpiod";
+
+    // Digital Output (GPIO) Provider name and unique ID
+    /**
+     * Constant <code>DIGITAL_OUTPUT_PROVIDER_NAME="NAME +   Digital Output (GPIO) Provider"</code>
+     */
+    public static final String DIGITAL_OUTPUT_PROVIDER_NAME = NAME + " Digital Output (GPIO) Provider";
+    /**
+     * Constant <code>DIGITAL_OUTPUT_PROVIDER_ID="ID + -digital-output"</code>
+     */
+    public static final String DIGITAL_OUTPUT_PROVIDER_ID = ID + "-digital-output";
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(PluginService service) {
+
+        Provider[] providers = {
+            GpioDDigitalOutputProvider.newInstance()
+        };
+
+        service.register(providers);
+    }
+}
---
+++ plugins/pi4j-plugin-gpiod/src/main/java/com/pi4j/plugin/gpiod/provider/gpio/digital/GpioDDigitalOutputProviderImpl.java
@@ -0,0 +1,63 @@
+package com.pi4j.plugin.gpiod.provider.gpio.digital;
+
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: PLUGIN   :: PIGPIO I/O Providers
+ * FILENAME      :  PiGpioDigitalOutputProviderImpl.java
+ *
+ * This file is part of the Pi4J project. More information about
+ * this project can be found here:  https://pi4j.com/
+ * **********************************************************************
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfig;
+import com.pi4j.io.gpio.digital.DigitalOutputProviderBase;
+import com.pi4j.library.pigpio.PiGpio;
+
+/**
+ * <p>PiGpioDigitalOutputProviderImpl class.</p>
+ *
+ * @author Robert Savage (<a href="http://www.savagehomeautomation.com">http://www.savagehomeautomation.com</a>)
+ * @version $Id: $Id
+ */
+public class GpioDDigitalOutputProviderImpl extends DigitalOutputProviderBase implements GpioDDigitalOutputProvider {
+
+    protected final PiGpio piGpio;
+
+    /**
+     * <p>Constructor for PiGpioDigitalOutputProviderImpl.</p>
+     *
+     */
+    public GpioDDigitalOutputProviderImpl(){
+        this.id = ID;
+        this.name = NAME;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public DigitalOutput create(DigitalOutputConfig config) {
+        // initialize the PIGPIO library
+        if(!piGpio.isInitialized()) piGpio.initialize();
+
+        // create new I/O instance based on I/O config
+        return new GpioDDigitalOutput(piGpio,this, config);
+    }
+}
