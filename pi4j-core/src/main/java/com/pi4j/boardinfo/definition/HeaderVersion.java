2da171d3cb5079ea10970533d77c96b9a93aa0c3
Frank Delporte <fdelporte@azul.com>
2024-04-17 11:29:21 +0200

    #340 Correct header for ZERO_PCB_1_2

--- pi4j-core/src/main/java/com/pi4j/boardinfo/definition/HeaderVersion.java
+++ pi4j-core/src/main/java/com/pi4j/boardinfo/definition/HeaderVersion.java
@@ -9,7 +9,7 @@ public enum HeaderVersion {
     PICO("Pico", "Used on the Pico microcontroller", new ArrayList<>()),
     TYPE_1("Type 1", "Used on original Model B", Collections.singletonList(HeaderPins.HEADER_26_TYPE_1)),
     TYPE_2("Type 2", "Used on Model A and Model B (revision 2)", Arrays.asList(HeaderPins.HEADER_26_TYPE_2, HeaderPins.HEADER_8)),
-    TYPE_3("Type 3", "Used on Model A+, B+, Pi Zero, Pi Zero W, Pi2B, Pi3B, Pi4B", Collections.singletonList(HeaderPins.HEADER_40)),
+    TYPE_3("Type 3", "Used on Model A+, B+, Pi Zero, Pi Zero W, Pi2B, Pi3B, Pi4B, Pi5B", Collections.singletonList(HeaderPins.HEADER_40)),
     COMPUTE("Compute Module", "54 GPIO", Arrays.asList(HeaderPins.COMPUTE_J5, HeaderPins.COMPUTE_J6)),
     UNKNOWN("Unknown", "", new ArrayList<>());
 
